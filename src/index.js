8fac422891017129d1a5ba585c16beca1eb339e2
Abeeb Ridwan Olumide <igortunde@yahoo.com>
2022-04-05 21:02:47 +0100

    build: removed linting package - they brought me nothing but headaches

--- src/index.js
+++ src/index.js
@@ -3,13 +3,12 @@ import ReactDOM from 'react-dom';
 import App from './app';
 import './normalize.css';
 import { GlobalStyle } from './global-styles';
-
+/* import { DataContext } from './context/data.js'; */
 
 ReactDOM.render(
   <React.StrictMode>
     <GlobalStyle />
-    <App />
+      <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById('root'),
 );
-

7de533690a90c82e18352c0b073619d4a4e8eeaf
Abeeb Ridwan Olumide <igortunde@yahoo.com>
2022-04-09 03:43:50 +0100

    fix: deprecated ReactDom in react 18 removed

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import App from './app';
 import './normalize.css';
 import { GlobalStyle } from './global-styles';
 
-ReactDOM.render(
+const rootElement = document.getElementById('root');
+const root = createRoot(rootElement);
+
+root.render(
   <React.StrictMode>
     <GlobalStyle />
-      <App />
+    <App />
   </React.StrictMode>,
   document.getElementById('root'),
 );

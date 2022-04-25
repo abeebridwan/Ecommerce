732d130e1fb206bcb2133911edbb287f35550bf3
Abeeb Ridwan <igortunde@yahoo.com>
2022-04-25 11:49:36 +0100

    refactor: font size adjusted

--- src/components/priceDropdown/style/priceDropdown.js
+++ src/components/priceDropdown/style/priceDropdown.js
@@ -29,7 +29,7 @@ export const Container = styled.ul`
   left: -2rem;
   font-weight: 500;
   line-height: 28.8px;
-  font-size: 1.8rem;
+  font-size: 1.7rem;
   box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);  
 `
 export const Item = styled.li`

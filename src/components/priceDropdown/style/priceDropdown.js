545010fc3b52948b363d8fc36877a3b416dfb513
Abeeb Ridwan <igortunde@yahoo.com>
2022-04-25 11:38:31 +0100

    refactor: hover effect spread over the whole line

--- src/components/priceDropdown/style/priceDropdown.js
+++ src/components/priceDropdown/style/priceDropdown.js
@@ -1,5 +1,4 @@
 import styled from 'styled-components/macro';
-import { Media } from '../../mixins'
 
 
 export const Frame = styled.div`
@@ -22,7 +21,6 @@ export const Container = styled.ul`
   display: ${({ priceshow }) => (priceshow ? "block" : "none")};
   list-style-type: none;
   margin: 0;
-  padding: 1rem 1.5rem 2rem 1rem;
   cursor: default;
   position: absolute;
   z-index: 11;
@@ -32,17 +30,12 @@ export const Container = styled.ul`
   line-height: 28.8px;
   font-size: 1.8rem;
   box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);  
-  ${Media('min-width', "500px", ['padding: 2rem 4rem 2rem 2rem'])};  
 `
 export const Item = styled.li`
-  margin: 2.1rem 0;
+  padding: 1.1rem 3rem 1.1rem 2rem;
+  display: block;
   cursor: pointer;
-  &:first-child{
-    margin-top: 0
-  }  
-  &:last-child{
-    margin-bottom: 0
-  }
+  
   &:hover{
     background-color: #1D1F22;
     color: #fff;

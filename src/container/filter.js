abe68049bf77bb8e51aa4ead6c900b014f8d9f3b
abeeb ridwan <olumideuae@gmail.com>
2022-11-01 09:10:30 +0100

    feat: handle selected change added and saved in localStorage, some refactor

--- src/container/filter.js
+++ src/container/filter.js
@@ -7,8 +7,8 @@ export default class FilterCom extends React.PureComponent {
     super(props);
     this.state = { 
       checked: this.initialState()[0],
-      attrId:  this.initialState()[1], 
-      selectValue: "",  
+      initAttrIds:  this.initialState()[1], 
+      selectedValues: {},  
       attributes: {}
     }
     this.handleCheck = this.handleCheck.bind(this);
@@ -52,9 +52,20 @@ export default class FilterCom extends React.PureComponent {
     console.log(attributes)
   }
   
-  handleChange(e){
-    console.log(e.target.value)
-    this.setState({selectValue: e.target.value });
+  handleChange(e, idValues){
+    console.log(e.target.value, "targetValue")
+    console.log(idValues, "idValues")
+    const { selectedValues } = this.state;
+    if(selectedValues[idValues[0]]){
+        selectedValues[idValues[0]][idValues[1]] = e.target.value
+     }else{
+      let attribute = {}
+      attribute[idValues[1]] = e.target.value
+      selectedValues[idValues[0]] = attribute
+    }
+    localStorage.setItem(`${idValues[2]}FilterSelectedAttr`, JSON.stringify(selectedValues));
+    this.setState({ selectedValues: { ...selectedValues } })
+    console.log(selectedValues)
   }
 
   handleCheck(e, idValues){
@@ -70,8 +81,8 @@ export default class FilterCom extends React.PureComponent {
   }
 
   render(){
-    const { products } = this.props.category;
-    const Methods = {handleCheck: this.handleCheck, handleChange: this.handleChange, 
+    const { name, products } = this.props.category;
+    const Methods = {categoryName: name, handleCheck: this.handleCheck, handleChange: this.handleChange, 
       handleColor: this.handleColor, matchAttr: this.matchAttr}
     return (
       <Filter onClick = {(e) => { this.props.filterMethod(this.props.filter) }}>
@@ -87,8 +98,8 @@ export default class FilterCom extends React.PureComponent {
                       <Filter.FilterItem key={attr.id}>
                         <Filter.FilterAttriText>{attr.name}:</Filter.FilterAttriText>
                         {
-                        this.state.attrId.includes(attr.id)?
-                        AttributesTemplate(item.id, attr, Methods, this.state, index= this.state.attrId.indexOf(attr.id)):
+                        this.state.initAttrIds.includes(attr.id)?
+                        AttributesTemplate(item.id, attr, Methods, this.state, index= this.state.initAttrIds.indexOf(attr.id)):
                         AttributesTemplate(item.id, attr, Methods, this.state)}
                       </Filter.FilterItem>
                     ))
@@ -103,7 +114,7 @@ export default class FilterCom extends React.PureComponent {
 }
 
 const AttributesTemplate =(productId, attr, Methods, state, index)=>{
-  const {handleCheck, handleChange, handleColor, matchAttr} = Methods
+  const {handleCheck, handleChange, handleColor, matchAttr, categoryName} = Methods
   let check = (item) => item.value === "Yes";
   let checkForYesOrNO = attr.items.some(check)
   let temp
@@ -121,11 +132,11 @@ const AttributesTemplate =(productId, attr, Methods, state, index)=>{
       </Filter.FilterAttributes> 
       break;
     default:
+      let idValues = [productId, attr.id, categoryName]
       if(checkForYesOrNO){
-        let idValues = [productId, attr.id]
         temp = <Filter.FilterCheckbox data-index={index} checked={state.checked[index]} onChange={(e)=>{handleCheck(e, idValues)}} type="checkbox" id={attr.id} name={attr.name} value={attr.items[attr.items.length - 1]['value']}/>
       }else{
-        temp = <Filter.FilterSelect name={attr.name} id={attr.id} value={state.selectValue} onChange={handleChange}>
+        temp = <Filter.FilterSelect name={attr.name} id={attr.id} onChange={(e) => {handleChange(e, idValues)}}>
             {attr.items.map((item)=>(
               <Filter.FilterOption key={item.id} value={item.value}>{item.displayValue}</Filter.FilterOption>
             ))}
